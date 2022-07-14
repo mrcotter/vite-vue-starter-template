97b3dcf5f6db17065a2da683cab097427cfad4f0
Kris Zheng <kris.zheng@cashwerkz.com>
2022-07-14 13:04:55 +1000

    chore: add copyright text back

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="layout-footer footer grid-flow-col items-center p-4 bg-base-200 dark:bg-neutral text-base-content dark:text-neutral-content">
     <div class="items-center grid-flow-col">
       <p class="px-1">
-        Copyright © {{ thisYear }}
+        Copyright © {{ thisYear }} - All right reserved
       </p>
     </div>
     <div class="grid-flow-col gap-4 justify-self-end">
